dda990060db7d925df081c6efe40a23a4563dea2
MHasan <tanviromi3@Tanvirs-MacBook-Pro-3.local>
2024-05-21 15:28:03 +0600

    source code linked added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Floating label inputbox with Next Js Formik Mantine" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Floating label input box</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
